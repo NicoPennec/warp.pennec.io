37760959af98c26dacbbe62e10e9183cc91c2086
NicoPennec <nemohp@yahoo.fr>
2020-03-11 23:13:12 +0100

    Update demo

--- src/index.js
+++ src/index.js
@@ -1,23 +1,89 @@
 'use strict'
 
-// import WarpJS
-import { defaultWarper as warper } from '@warpjs/warp'
+import { defaultWarper as warp } from '@warpjs/warp'
 import engine from '@warpjs/engine'
 
-// init WarpJS
 engine.init()
 
-// warp function
+/** Say hello */
 const hello = name => {
-  // warp directive
   'warp +server -client'
 
-  return `Hello ${name} from Node.js ${process.version}`
+  return `Hello World!`
 }
 
-// on load
+/** Get cpu/mem usage */
+const usage = () => {
+  'warp +server -client'
+
+  const os = require('os')
+
+  return {
+    cpus: os.cpus(),
+    totalmem: os.totalmem(),
+    freemem: os.freemem(),
+    uptime: os.uptime()
+  }
+}
+
+const result = document.getElementById('result')
+const stats = document.getElementById('stats')
+
+/** Refresh stats */
+const refresh = async () => {
+  const data = await warp.call(usage)
+  const cpus = data.cpus.map(cpu => {
+    const data = {}
+    let total = 0
+
+    for (var type in cpu.times) {
+      total += cpu.times[type]
+    }
+    for (type in cpu.times) {
+      data[type] = total ? Math.round((100 * cpu.times[type]) / total) : 0
+    }
+    data.speed = cpu.speed
+    return data
+  })
+
+  const uptime = data.uptime
+  const freemem = data.freemem / 1024 ** 3
+  const totalmem = data.totalmem / 1024 ** 3
+  const usedmem = (totalmem - freemem).toFixed(2)
+
+  stats.innerHTML = `<hr>
+  <ul>
+    <li>
+      Uptime: <b>${uptime} sec</b>
+      <br>
+      <progress>
+    </li>
+    <li>
+      Memory: <b>${usedmem} Go</b> / <b>${totalmem} Go</b>
+      <br>
+      <progress max="100" value="${(usedmem * 100) /
+        totalmem}">${usedmem} Go</progress>
+    <li>
+    ${cpus
+      .map(
+        ({ user, speed }, index) =>
+          `<li>
+            CPU ${index + 1}: <b>${String(user).padStart(
+            2,
+            ' '
+          )} % (${speed} Mhz)</b>
+            <br>
+            <progress max="100" value="${user}">${user}%</progress>
+          </li>`
+      )
+      .join('')}
+  </ul>`
+}
+
+// on page load
 window.addEventListener('DOMContentLoaded', async () => {
-  // call function with WarpJS
-  const response = await warper.call(hello, 'World')
-  document.getElementById('result').innerHTML = response
+  const message = await warp.call(hello, 'World')
+  result.innerHTML = message
+  refresh()
+  setInterval(refresh, 3000)
 })
--- webpack.config.js
+++ webpack.config.js
@@ -7,34 +7,21 @@ require('dotenv').config()
 
 module.exports = {
   plugins: [
-    // clean output for each build
     new CleanWebpackPlugin(),
-    // define html template
     new HtmlWebpackPlugin({
       template: 'src/index.html'
     }),
-    // copy assets to output
     new CopyPlugin([
       {
         from: 'public',
         to: '.'
       }
     ]),
-    // WarpJS webpack plugin
     new WarpifyPlugin({
-      exclude: [/node_modules/],
-      include: [/\.js$/],
       config: {
-        // project config
         project: {
           userId: process.env.WARPJS_USER_ID,
           name: process.env.WARPJS_PROJECT_NAME
-        },
-        // server initialization
-        server: {
-          init: {
-            import: './server-init.js'
-          }
         }
       }
     })
